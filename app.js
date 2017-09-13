9f3e6ddc3e05a9918938428da213f75049047a58
Christopher Harrop <bannastre@gmail.com>
2017-09-13 12:07:22 +0100

    identify req with body parser

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
@@ -9,6 +13,7 @@ app.get('/', (req, res) => {
 
 app.post('/hours', (req, res) => {
   console.log('------HARVEST--------');
+  console.log('req: ', req);
   console.log('req body: ', req.body);
   console.log('req body.text: ', req.body.text);
   console.log('------HARVEST--------');
