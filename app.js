2c4c29d8d9fc588580ceb27d6db70e80b0c8f08d
Christopher Harrop <bannastre@gmail.com>
2017-09-13 12:03:13 +0100

    identify req.body

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.get('/', (req, res) => {
 
 app.post('/hours', (req, res) => {
   console.log('------HARVEST--------');
-  console.log('req data: ', req.data);
+  console.log('req body: ', req.body);
+  console.log('req body.text: ', req.body.text);
   console.log('------HARVEST--------');
   res.send("Got a post request... so that's good");
 });
